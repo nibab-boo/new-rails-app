ffff67e6d5f53980f521f054ad99e64e2b8ad3e7
Babin Bohora <bohorababin1@gmail.com>
2022-01-04 18:08:10 +0900

    shadowing done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,14 @@
 class PagesController < ApplicationController
+  def about
+  end
+
+  def contact
+    @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+    if params[:member].present?
+      @members = @members.select{ |name| name.downcase.start_with?(params[:member])}
+    end
+  end
+
+  def home
+  end
 end
